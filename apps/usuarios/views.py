f3a15541db841c53d50a15a13e860173db418b8e
lyansad <ysaaccolman@gmail.com>
2023-12-18 21:30:29 -0300

    se crea la app art

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import logout
 from django.shortcuts import redirect
-from django.views import View
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
@@ -11,7 +10,8 @@ class Registro(CreateView):
     success_url = reverse_lazy('login')
     template_name = 'usuarios/registro.html'
 
-class LogoutView(View):
+class LogoutView(CreateView):
     def post(self, request):
         logout(request)
         return redirect('home')
+
